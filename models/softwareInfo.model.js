cb7474253f6c5b15738f0d792785aa2ac5d1fe82
Akshitha Lingannapeta <alingannapeta@evoketechnologies.com>
2021-08-04 19:37:13 +0530

    update api for billingdetails

--- models/softwareInfo.model.js
+++ models/softwareInfo.model.js
@@ -3,22 +3,28 @@ const Schema = mongoose.Schema;
 const softwareInfoSchema = new Schema(
     {
         softwareName: { type: String },
-        teamName: { type: String },
-        selectType: { type: String },
+        softwareType: { type: String },
         owner: { type: String },
-        pricingInDollar: { type: String },
-        pricingInRupee: { type: String },
+        team: { type: String },
         totalAmount: { type: String },
-        timeline: { type: String },
-        billingCycle: { type: String},
-        nextBilling: { type: Date}
-        // deleteReason: { type: String },
+        billingCycle: { type: String },
+        nextBilling: { type: Date },
+        billingDetails: [{
+            pricingInDollar: { type: String },
+            pricingInRupee: { type: String },
+            billingMonth: { type: String },
+            nextBilling: { type: Date },
+            createdAt: { type: Date },
+            timeline: { type: String },
+        }],
+        isDelete: Boolean,
+        deleteReason: { type: String },
         // restoreReason: { type: String },
         // reshareReason: { type: String },
-    }, 
-    
-    {  timestamps: true }
+    },
+
+    { timestamps: true }
 );
 
 const SoftwareInfo = mongoose.model('SoftwareInfo', softwareInfoSchema);
-module.exports = SoftwareInfo; 
+module.exports = SoftwareInfo;
--- routes/softwareInfo.js
+++ routes/softwareInfo.js
@@ -3,19 +3,18 @@ let SoftwareInfo = require("../models/softwareInfo.model");
 const log = console.log;
 
 
-router.route("/addNewSoftware").post((req, res) => {
+router.route("/create").post((req, res) => {
   const softwareName = req.body.softwareName;
   const teamName = req.body.teamName;
   const selectType = req.body.selectType;
   const owner = req.body.owner;
   const pricingInDollar = req.body.pricingInDollar;
-  const pricingInRupee =  req.body.pricingInRupee;
-  const totalAmount =  req.body.totalAmount;
+  const pricingInRupee = req.body.pricingInRupee;
+  const totalAmount = req.body.totalAmount;
   const timeline = req.body.timeline;
   const billingCycle = req.body.billingCycle;
-  const nextBilling = Date.parse( req.body.nextBilling);
-
-  // const deleteReason = "";
+  const nextBilling = Date.parse(req.body.nextBilling);
+  const deleteReason = req.body.deleteReason
   // const restoreReason = "";
   // const reshareReason = "";
 
@@ -29,8 +28,8 @@ router.route("/addNewSoftware").post((req, res) => {
     totalAmount,
     timeline,
     billingCycle,
-    nextBilling
-    // deleteReason,
+    nextBilling,
+    deleteReason,
     // restoreReason,
     // reshareReason,
 
@@ -38,8 +37,8 @@ router.route("/addNewSoftware").post((req, res) => {
 
   newSoftwareInfo
     .save()
-    .then((row) => {   
-      res.json("success");  
+    .then((row) => {
+      res.json("success");
       log("Form saved to mongo with the ID : ", row._id);
     })
     .catch((err) => {
@@ -48,9 +47,10 @@ router.route("/addNewSoftware").post((req, res) => {
       });
       log("Error in saving the form to mongodb : ", err)
     });
-    
+
 });
 
+// Get all software info data
 router.route("/").get((req, res) => {
   SoftwareInfo.find().sort({
     createdAt: -1,
@@ -59,31 +59,29 @@ router.route("/").get((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+// Get all software info data by id
 router.route("/:id").get((req, res) => {
   SoftwareInfo.findById(req.params.id)
     .then((softwareInfo) => res.json(softwareInfo))
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+// Get record by id and update it
+router.route("/update/:id").post((req, res) => {
+  const payload = req.body
+  log('payload', payload)
+  SoftwareInfo.findByIdAndUpdate(req.params.id, payload, { upsert: true })
+    .then(() => res.json("Status is updated to Deleted!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+
+// Get record by id and delete it
 router.route("/:id").delete((req, res) => {
   SoftwareInfo.findByIdAndDelete(req.params.id)
     .then(() => res.json("SoftwareInfo deleted."))
     .catch((err) => res.status(400).json("Error: " + err));
 });
-
-router.route("/updateStatus/:id").post((req, res) => {
-  SoftwareInfo.findById(req.params.id)
-    .then((softwareInfo) => {
-      softwareInfo.deleteReason = req.body.deleteReason;
-      softwareInfo.status = req.body.status;
-      softwareInfo
-        .save()
-        .then(() => res.json("Status is updated to Deleted!"))
-        .catch((err) => res.status(400).json("Error: " + err));
-    })
-    .catch((err) => res.status(400).json("Error: " + err));
-});
-
 
 
 module.exports = router;
